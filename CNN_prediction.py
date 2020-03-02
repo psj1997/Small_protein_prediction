9c9607d59e8f721596085afd7b84fb0484879157
psj1997 <1176894699@qq.com>
2020-03-02 11:24:07 +0800

    update

--- CNN_prediction.py
+++ CNN_prediction.py
@@ -1,4 +1,12 @@
-from keras.models import Sequential, load_model, model_from_config
+from keras.models import Sequential, load_model, model_from_config,Input,Model
+from keras.layers.merge import Concatenate
+import os
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split,StratifiedKFold,GroupKFold
+import json
+from sklearn.metrics import recall_score, precision_score, accuracy_score, f1_score
 
 MAX_SEQ_LENGTH = 153
 
@@ -27,11 +35,18 @@ def sequence_to_onehot(seq):
     seq_one_hot = np.array(seq_one_hot).reshape(153,4)
     return seq_one_hot
 
-from keras.layers import Conv1D,MaxPool2D,MaxPool1D,Flatten,Dense
+from keras.layers import Conv1D,MaxPool2D,MaxPool1D,Flatten,Dense,Dropout,LSTM
 
-def CNN_model():
+def feature_extractor(inp,kernel_size,num_filter):
+    out = Conv1D(filters=num_filter,kernel_size=kernel_size,strides=1,activation='relu',padding='same', data_format='channels_last',input_shape=(153,4))(inp)
+    out = MaxPool1D(pool_size=5,strides=1,padding='same')(out)
+    out = Flatten()(out)
+    return out
+
+
+def CNN_model_oneconv(kernel_size,num_filter):
     model = Sequential()
-    model.add(Conv1D(filters=1,kernel_size=3,strides=1,activation='relu',padding='valid', data_format='channels_last',input_shape=(153,4)))
+    model.add(Conv1D(filters=num_filter,kernel_size=kernel_size,strides=1,activation='relu',padding='valid', data_format='channels_last',input_shape=(153,4)))
     model.add(MaxPool1D(pool_size=5,strides=1,padding='valid'))
     model.add(Flatten())
     model.add(Dense(64,activation='relu'))
@@ -40,54 +55,140 @@ def CNN_model():
     model.summary()
     return model
 
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split,StratifiedKFold
-import json
+def CNN_model_multi_conv(kernel_size,num_filter):
+    inp = Input(shape=(153,4))
+    conv_block = []
+    for size in kernel_size:
+        out = feature_extractor(inp,size,num_filter)
+        conv_block.append(out)
+    conv_out = Concatenate()(conv_block)
+    Dense_out = Dense(512,activation='relu')(conv_out)
+    Drop_out = Dropout(0.5)(Dense_out)
+    Dense_out = Dense(64,activation='relu')(Drop_out)
+    Drop_out = Dropout(0.5)(Dense_out)
+    model_output = Dense(2,activation='softmax')(Drop_out)
+    model = Model(inp, model_output)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    return model
 
-if __name__ == '__main__':
-    # seq = sequence_to_onehot('ATGC')
-    # print(len(seq))
-    #CNN_model()
-    import os
+
+def feature_extractor_LSTM(inp,kernel_size,num_filter):
+    out = Conv1D(filters=num_filter,kernel_size=kernel_size,strides=1,activation='relu',padding='same', data_format='channels_last',input_shape=(153,4))(inp)
+    out = MaxPool1D(pool_size=5,strides=1,padding='same')(out)
+    #out = Flatten()(out)
+    return out
+
+
+def CNN_lstm_model(kernel_size,num_filter):
+    inp = Input(shape=(153,4))
+    conv_block = []
+    for size in kernel_size:
+        out = feature_extractor_LSTM(inp,size,num_filter)
+        conv_block.append(out)
+    conv_out = Concatenate()(conv_block)
+    lstm_out = LSTM(96, dropout=0.4, recurrent_dropout=0.4, return_sequences=True)(conv_out)
+    lstm_out = LSTM(64, recurrent_dropout=0.4)(lstm_out)
+    Drop_out = Dropout(0.5)(lstm_out)
+    model_output = Dense(2,activation='softmax')(Drop_out)
+    model = Model(inp, model_output)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    return model
+
+
+
+
+def Cross_validation(is_group = False):
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    training_data = pd.read_csv('/home1/pansj/Small_protein_prediction/training_data_whole.csv',index_col=0).values
-    data = training_data[:,0]
-    label = training_data[:,1]
-    label = label.reshape(len(label),-1)
+    training_data = pd.read_csv('/home1/pansj/Small_protein_prediction/training_data_cluster.csv',index_col=0).values
+    print(training_data.shape)
+    data = training_data[:, 0]
+    label = training_data[:, 1]
+    cluster = training_data[:, 3]
+    print('cluster:', len(set(cluster)))
+    label = label.reshape(len(label), -1)
+    print(np.sum(label > 0), len(label))
     label[label == -1] = 0
-    label = np.array([y[0] for y in label]).reshape(len(label),1)
+    label = np.array([y[0] for y in label]).reshape(len(label), 1)
     data_one_hot = []
     print('start')
     for seq in data:
         seq_one_hot = sequence_to_onehot(seq)
         data_one_hot.append(seq_one_hot)
-    #print(np.array(data_one_hot).shape)
+    # print(np.array(data_one_hot).shape)
     train_data = np.array(data_one_hot)
-    skf = StratifiedKFold(n_splits=5,shuffle=True)
 
     accuracy = []
-    for train_index,test_index in skf.split(train_data,label):
-        X_train,Y_train = train_data[train_index] , label[train_index]
-        X_test, Y_test = train_data[test_index] , label[test_index]
-        enc = OneHotEncoder()
-        Y_train = enc.fit_transform(Y_train).toarray()
-        Y_test = enc.fit_transform(Y_test).toarray()
+    recall = []
+    precision = []
+    f1 = []
 
-        cnn_model = CNN_model()
-        cnn_model.fit(X_train,Y_train,batch_size=64,epochs=5)
-        #prediction = cnn_model.predict(X_test)
+    if is_group == False:
+        print('CV')
 
-        #print(prediction)
-        score = cnn_model.evaluate(X_test,Y_test,verbose=0)
-        print(score[1])
-        #accuracy.append(score)
-        accuracy.append(score[1])
+        skf = StratifiedKFold(n_splits=5, shuffle=True)
+        for train_index, test_index in skf.split(train_data, label):
+            X_train, Y_train = train_data[train_index], label[train_index]
+            X_test, Y_test = train_data[test_index], label[test_index]
+            enc = OneHotEncoder()
+            Y_train = enc.fit_transform(Y_train).toarray()
 
+            cnn_model = CNN_lstm_model([3, 4, 5], 32)
+            cnn_model.fit(X_train, Y_train, batch_size=128, epochs=10)
+            pre_y = []
+            pre = cnn_model.predict(X_test)
+            for i in range(len(pre)):
+                if pre[i][0] > pre[i][1]:
+                    pre_y.append(0)
+                else:
+                    pre_y.append(1)
+            pre_y = np.array(pre_y).reshape(len(pre_y), 1)
+            accuracy.append(accuracy_score(Y_test, pre_y))
+            recall.append(recall_score(Y_test, pre_y))
+            precision.append(precision_score(Y_test, pre_y))
+            f1.append(f1_score(Y_test, pre_y))
+    else:
+        print('group_cv')
+        skf = GroupKFold(n_splits=5)
+
+        for train_index, test_index in skf.split(train_data, label, cluster):
+            print(len(train_index), len(test_index))
+            X_train, Y_train = train_data[train_index], label[train_index]
+            X_test, Y_test = train_data[test_index], label[test_index]
+            enc = OneHotEncoder()
+            Y_train = enc.fit_transform(Y_train).toarray()
+            # Y_test = enc.fit_transform(Y_test).toarray()
+
+            cnn_model = CNN_model_multi_conv([3, 4, 5], 10)
+            cnn_model.fit(X_train, Y_train, batch_size=128, epochs=10)
+            pre_y = []
+            pre = cnn_model.predict(X_test)
+            for i in range(len(pre)):
+                if pre[i][0] > pre[i][1]:
+                    pre_y.append(0)
+                else:
+                    pre_y.append(1)
+            pre_y = np.array(pre_y).reshape(len(pre_y), 1)
+            accuracy.append(accuracy_score(Y_test, pre_y))
+            recall.append(recall_score(Y_test, pre_y))
+            precision.append(precision_score(Y_test, pre_y))
+            f1.append(f1_score(Y_test, pre_y))
+
+    print('CNN: kernel size:[3,4,5] ; num_kernel = 10')
     print(accuracy)
-    print(np.average(accuracy))
+    print('accuracy: ', np.average(accuracy))
+    print(recall)
+    print('recall:', np.average(recall))
+    print(precision)
+    print('precision:', np.average(precision))
+    print(f1)
+    print('f1:', np.average(f1))
+
+
+if __name__ == '__main__':
+    Cross_validation(is_group=True)
 
 
 
--- kmer.py
+++ kmer.py
@@ -4,63 +4,98 @@ using 3mer to build a easy baseline model of the small protein prediction
 import numpy as np
 import pandas as pd
 from itertools import product
-codon_list = [''.join(cs) for cs in product('ATCG', 'ATCG', 'ATCG')]
+from sklearn.model_selection import train_test_split, StratifiedKFold,GroupKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import recall_score, precision_score, accuracy_score, f1_score
+
+codon_list = [''.join(cs) for cs in product('ATCG', 'ATCG', 'ATCG','ATCG','ATCG')]
 codon2ix = {c:i for i,c in enumerate(codon_list)}
 
-def seq_to_vector(seq):
-    vector = np.zeros(64)
-    for i in range(len(seq)-3):
+def seq_to_vector(seq,k):
+    vector = np.zeros(4**k)
+    for i in range(len(seq)-k):
         #print(seq[i:i+3])
-        ix = codon2ix.get(seq[i:i+3])
+        ix = codon2ix.get(seq[i:i+k])
         if ix is not None:
             vector[ix] += 1
     return vector
 
-training_data = pd.read_csv('data/training_data_whole.csv',index_col=0).values
-data = training_data[:,0]
+def Cross_validation(is_group = False):
+    training_data = pd.read_csv('/home1/pansj/Small_protein_prediction/training_data_cluster.csv',index_col=0).values
+    print(training_data.shape)
+    data = training_data[:, 0]
+    label = training_data[:, 1]
+    cluster = training_data[:, 3]
+    print('cluster:', len(set(cluster)))
+    label = label.reshape(len(label), -1)
+    print(np.sum(label > 0), len(label))
+    label[label == -1] = 0
+    label = np.array([y[0] for y in label]).reshape(len(label), 1)
 
-label = training_data[:,1]
-label = label.reshape(len(label),-1)
-label[label == -1] = 0
-label = np.array([y[0] for y in label]).reshape(len(label),1)
+    train_data = np.array([seq_to_vector(seq, 5) for seq in data])
+    print(len(train_data))
 
-train_data = np.array([seq_to_vector(seq) for seq in data])
+    accuracy = []
+    recall = []
+    precision = []
+    f1 = []
 
-from sklearn.model_selection import train_test_split,StratifiedKFold
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-skf = StratifiedKFold(n_splits=5, shuffle=True)
-from collections import Counter
-accuracy = []
-recall = []
-precision = []
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import recall_score,precision_score
-for train_index, test_index in skf.split(train_data, label):
-    X_train, Y_train = train_data[train_index], label[train_index]
-    X_test, Y_test = train_data[test_index], label[test_index]
+    if is_group == False:
+        print('CV:')
+        scaler = StandardScaler()
+        skf = StratifiedKFold(n_splits=5, shuffle=True)
+        for train_index, test_index in skf.split(train_data, label):
+            print(len(train_index),len(test_index))
+            X_train, Y_train = train_data[train_index], label[train_index]
+            X_test, Y_test = train_data[test_index], label[test_index]
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.fit_transform(X_test)
+            clf = RandomForestClassifier()
+            #clf = LogisticRegression()
+            clf.fit(X_train,Y_train)
+            acc = clf.score(X_test,Y_test)
+            pre = clf.predict(X_test)
+            accuracy.append(acc)
+            recall.append(recall_score(Y_test,pre))
+            precision.append(precision_score(Y_test,pre))
+            f1.append(f1_score(Y_test,pre))
+    else:
+        print('Group_cv')
+        scaler = StandardScaler()
+        skf = GroupKFold(n_splits=5)
+        for train_index, test_index in skf.split(train_data, label, cluster):
+            print(len(train_index), len(test_index))
+            X_train, Y_train = train_data[train_index], label[train_index]
+            X_test, Y_test = train_data[test_index], label[test_index]
 
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.fit_transform(X_test)
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.fit_transform(X_test)
 
-    #clf = RandomForestClassifier()
-    clf = LogisticRegression()
-    clf.fit(X_train,Y_train)
-    acc = clf.score(X_test,Y_test)
-    pre = clf.predict(X_test)
+            clf = RandomForestClassifier()
+            # clf = LogisticRegression()
+            clf.fit(X_train, Y_train)
+            acc = clf.score(X_test, Y_test)
+            pre = clf.predict(X_test)
+
+            accuracy.append(acc)
+            recall.append(recall_score(Y_test, pre))
+            precision.append(precision_score(Y_test, pre))
+            f1.append(f1_score(Y_test, pre))
+
+    print(accuracy)
+    print('acc:',np.average(accuracy))
+    print(recall)
+    print('recall:',np.average(recall))
+    print(precision)
+    print('precision:',np.average(precision))
+    print(f1)
+    print('f1:',np.average(f1))
 
 
-    accuracy.append(acc)
-    recall.append(recall_score(Y_test,pre))
-    precision.append(precision_score((Y_test,pre)))
-
-print(accuracy)
-print('acc:',np.average(accuracy))
-print(recall)
-print('recall:',np.average(recall))
-print(precision)
-print('precision:',np.average(precision))
+if __name__ == '__main__':
+    Cross_validation(is_group=True)
 
 
 
